dfb5f10200d30bdb5ce41c113a59f25e68ec7652
sselvanathan <senthuranselvanathanbusiness@gmail.com>
2020-05-29 22:20:26 +0200

    Add Functionality to save blogPosts in db

---
+++ src/Controllers/Blog/BlogController.php
@@ -0,0 +1,57 @@
+<?php
+
+    declare(strict_types=1);
+
+    namespace Controllers\Blog;
+
+    use Common\Controller;
+    use Common\ControllerInterface;
+    use Database\Config\EntityManager;
+    use Database\Entities\BlogEntity;
+    use DateTime;
+    use Doctrine\ORM\OptimisticLockException;
+    use Doctrine\ORM\ORMException;
+
+    class BlogController extends Controller implements ControllerInterface
+    {
+        public function __construct()
+        {
+            parent::__construct(
+                [
+                    $this->setTemplatePath('home'),
+                    $this->setTemplateData(
+                        ['module' => 'Blog']
+                    ),
+                ]
+            );
+        }
+
+        public function getTwigData()
+        {
+            // TODO: Implement getTwigData() method.
+        }
+
+        public function createBlogPost(string $title, string $post): void
+        {
+            $blog = new BlogEntity();
+            $blog->setTitle($title);
+            $blog->setPost($post);
+            $blog->setCreatedAt(new DateTime('now'));
+
+            $entityManager = (new EntityManager)->createEntityManager();
+
+            try {
+                $entityManager->persist($blog);
+            } catch (ORMException $e) {
+                echo $e;
+            }
+
+            try {
+                $entityManager->flush();
+            } catch (OptimisticLockException | ORMException $e) {
+                echo $e;
+            }
+
+            echo "Der Blog mit der ID " . $blog->getId() . " wurde erstellt";
+        }
+    }
